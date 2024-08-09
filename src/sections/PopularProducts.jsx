ee3a29449511a7059dede6f31927a6f668a05fea
alexajaime09 <alexaquezada23@gmail.com>
2024-08-09 13:55:03 -0600

    HERO

---
+++ src/sections/PopularProducts.jsx
@@ -0,0 +1,11 @@
+
+
+const PopularProducts = () => {
+  return (
+    <div>
+      Popular
+    </div>
+  )
+}
+
+export default PopularProducts
---
+++ src/sections/SpecialOffer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SpecialOffer = () => {
+  return (
+    <div>
+      speacial
+    </div>
+  )
+}
+
+export default SpecialOffer
